9c60e2d993e6725e4ea94fb37c637ffe42114006
Dawid <dcieszynski99@gmail.com>
2022-05-25 09:21:04 +0200

    dungeon fighter nft init

---
+++ scripts/run.js
@@ -0,0 +1,20 @@
+const main = async () => {
+  const dungeonFighterFactory = await hre.ethers.getContractFactory(
+    "DungeonFighter"
+  );
+  const dungeonFighterContract = await dungeonFighterFactory.deploy();
+  await dungeonFighterContract.deployed();
+  console.log("Contract deployed to: ", nftGameContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log("Error: ", error);
+    process.exit(1);
+  }
+};
+
+runMain();
